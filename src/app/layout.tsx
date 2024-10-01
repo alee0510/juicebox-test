5022ccb89c15973d2a42b745868fbd3229ad3325
Ali Muksin <ali.muksin0510@gmail.com>
2024-10-02 03:57:10 +0700

    chore: implement local fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { fontVariables } from "@/app/fonts";
 import "./globals.css";
 
-
 export const metadata: Metadata = {
   title: "Juicebox",
   description: "demo test for juicebox",
@@ -16,9 +15,7 @@ export default function RootLayout({
   console.log(fontVariables);
   return (
     <html lang="en">
-      <body className={fontVariables}>
-        {children}
-      </body>
+      <body className={`${fontVariables}`}>{children}</body>
     </html>
   );
 }
