a29d1e50d98685a3d052c363f9053a6d21bef9d2
Ali Muksin <ali.muksin0510@gmail.com>
2024-10-02 05:35:56 +0700

    chore: create basic page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
+"use client";
+
+import clsx from "clsx";
+import Header from "@/app/_lib/components/features/header";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
-
+    <div className={styles.container}>
+      <Header />
+      <section className={clsx(styles.section, styles["section-1"])}>
+        <button className={styles.button}>to section 01</button>
+      </section>
+      <section className={clsx(styles.section, styles["section-2"])}>
+        <button className={styles.button}>to section 01</button>
+      </section>
+      <section className={clsx(styles.section, styles["section-3"])}>
+        <button className={styles.button}>to section 01</button>
+      </section>
     </div>
   );
 }
