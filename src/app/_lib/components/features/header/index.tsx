6b461da33fb90cae92e55d8563407ff9134825c7
Ali Muksin <ali.muksin0510@gmail.com>
2024-10-02 05:34:37 +0700

    chore: create header component

---
+++ src/app/_lib/components/features/header/index.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import styles from "@/app/_lib/components/features/header/styles.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <button aria-label="back-button" className={styles["icon-button"]}>
+        <Image src="/icons/arrow-left.svg" alt="back" priority width={20} height={20} />
+      </button>
+      <div aria-label="logo" className={styles.logo}>
+        <Image src="/images/logo.svg" priority alt="logo" sizes="100%" width={123} height={29} />
+      </div>
+      <button aria-label="refresh-button" className={styles["icon-button"]}>
+        <Image src="/icons/refresh.svg" alt="refresh" priority width={20} height={20} />
+      </button>
+    </header>
+  );
+}
