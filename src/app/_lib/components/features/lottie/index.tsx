daef1a87f1d8ab884d7fd89a3c67c1a58ee0ef08
Ali Muksin <ali.muksin0510@gmail.com>
2024-10-02 15:36:32 +0700

    chore: fixed autoplay lottie file

--- src/app/_lib/components/features/lottie/index.tsx
+++ src/app/_lib/components/features/lottie/index.tsx
@@ -1,19 +1,25 @@
-import Lottie from "lottie-react";
+import { MutableRefObject } from "react";
+import Lottie, { LottieRefCurrentProps } from "lottie-react";
 import AnimationFile from "@/../public/lotties/JB2G_Lottie.json";
 import styles from "./lottie.module.css";
 
+// @props
+type LottieProps = {
+  componentRef?: MutableRefObject<LottieRefCurrentProps | null>;
+};
+
 // @main component
-export default function LottieAnimation() {
+export default function LottieAnimation({ componentRef }: LottieProps): JSX.Element {
   return (
     <div className={styles["container"]}>
       <div className={styles["lottie"]}>
         <Lottie
           loop={false}
-          autoPlay={false}
+          autoplay={false}
+          lottieRef={componentRef}
           animationData={AnimationFile}
           aria-label="lottie animation"
           style={{ zIndex: -1 }}
-          //   lottieRef={lottie}
         />
       </div>
     </div>
