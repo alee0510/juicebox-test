e1195e7e8670d8b2c8f9a4421983cf4fae8c29bf
Ali Muksin <ali.muksin0510@gmail.com>
2024-10-02 13:24:10 +0700

    refactor: home screen into home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,37 @@
 "use client";
 
-import Lottie from "lottie-react";
-
-// import clsx from "clsx";
-import Header from "@/app/_lib/components/features/header";
-import AnimationFile from "../../public/lotties/JB2G_Lottie.json";
+// @components & hooks
+import { useRouter } from "next/navigation";
+import Button from "@/app/_lib/components/ui/button";
 import styles from "./page.module.css";
 
-// @screens
-// import Button from "./_lib/components/ui/button";
-// import Input from "./_lib/components/ui/input";
-// import HomeScreen from "@/app/_lib/screens/home";
-// import TutorialScreen from "./_lib/screens/tutorial";
-// import FormScreen from "./_lib/screens/form";
-import ResultScreen from "./_lib/screens/results";
-
-export default function Home() {
+// @main component
+export default function HomePage(): JSX.Element {
+  const router = useRouter();
   return (
-    <div className={styles.container}>
-      <Header />
-      <div className={styles.decorator}>
-        <div className={styles.lottie}>
-          <Lottie animationData={AnimationFile} style={{ zIndex: -1 }} />
-        </div>
+    <section className={styles["section"]}>
+      <div className={styles["info-container"]}>
+        <p>WA businesses feel confident about future growth</p>
+        <p className={styles["align-right"]}>AI cant replace creativity</p>
+        <p>Sales measure true success</p>
+        <p className={styles["align-right"]}>Human connection drives WA business</p>
+        <p>The primary barrier to digital transformation is financial investment</p>
       </div>
-      {/* <HomeScreen /> */}
-      {/* <TutorialScreen /> */}
-      {/* <FormScreen /> */}
-      <ResultScreen />
-    </div>
+      <div className={styles["content-container"]}>
+        <h2 className={styles["content-heading"]}>
+          Compare your thoughts on <span className={styles["gradient-text"]}>technology</span> with
+          current industry opinions.
+        </h2>
+      </div>
+      <div className={styles["content-container"]}>
+        <Button
+          title="Get a reality check"
+          name="check-reality-button"
+          type="filled"
+          color="primary"
+          onClick={() => router.push("/tutorial")}
+        />
+      </div>
+    </section>
   );
 }
