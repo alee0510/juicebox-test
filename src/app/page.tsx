8c28c87600a8800525a1dd0bd2fd9af4001c43f2
Ali Muksin <ali.muksin0510@gmail.com>
2024-10-02 09:51:05 +0700

    chore: implement result layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,18 @@
 
 import Lottie from "lottie-react";
 
-import clsx from "clsx";
+// import clsx from "clsx";
 import Header from "@/app/_lib/components/features/header";
 import AnimationFile from "../../public/lotties/JB2G_Lottie.json";
 import styles from "./page.module.css";
 
 // @screens
-import Button from "./_lib/components/ui/button";
-import Input from "./_lib/components/ui/input";
-import HomeScreen from "@/app/_lib/screens/home";
-import TutorialScreen from "./_lib/screens/tutorial";
-import FormScreen from "./_lib/screens/form";
+// import Button from "./_lib/components/ui/button";
+// import Input from "./_lib/components/ui/input";
+// import HomeScreen from "@/app/_lib/screens/home";
+// import TutorialScreen from "./_lib/screens/tutorial";
+// import FormScreen from "./_lib/screens/form";
+import ResultScreen from "./_lib/screens/results";
 
 export default function Home() {
   return (
@@ -25,7 +26,8 @@ export default function Home() {
       </div>
       {/* <HomeScreen /> */}
       {/* <TutorialScreen /> */}
-      <FormScreen />
+      {/* <FormScreen /> */}
+      <ResultScreen />
     </div>
   );
 }
